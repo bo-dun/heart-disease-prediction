17d323c0af804fb4fe932c9cd2b535741543bbf7
Peter Dun <bodun@stanford.edu>
2017-12-10 23:04:04 -0800

    updated matrix plot

--- generate_matrix.py
+++ generate_matrix.py
@@ -4,9 +4,9 @@ labels = ['Healthy', 'Heart Disease']
 matrix = [[57, 24],[17, 82]]
 fig = plt.figure()
 ax = fig.add_subplot(111)
-cax = ax.matshow(matrix)
+cax = ax.matshow(matrix, cmap='Reds')
 plt.title('Random Forest Confusion Matrix')
-fig.colorbar(cax)
+fig.colorbar(cax, boundaries=range(5,100))
 ax.set_xticklabels([''] + labels)
 ax.set_yticklabels([''] + labels)
 plt.xlabel('Predicted')
